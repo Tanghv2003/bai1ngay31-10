de9f8c34b9b45128ea0026825b76603427f432e3
tanghv <tanghv.info@gmail.com>
2024-10-31 11:33:00 +0700

    add

--- app/src/main/java/com/example/happy/MainActivity.kt
+++ app/src/main/java/com/example/happy/MainActivity.kt
@@ -1,163 +1,67 @@
 package com.example.happy
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.MenuItem
-import android.view.View
 import android.widget.*
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.happy.ui.theme.HappyTheme
-import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var tvDisPlay: TextView;
-    var cInput : String = "";//
-    var op : String? = null;// toan tu
-    var op1 : Int? = null;// toan hang 1
-    var op2 : Int? = null;// toan hang 2
-
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.doitien)
+        setContentView(R.layout.bai1ngay31thang10)
 
-        val vietnam = mapOf("US" to 1/25641f, "Viet Nam" to 1.0/1f, "Japan" to 1.0/166f, "Korea" to 1.0/18f)
-        val korea = mapOf("US" to 72.0/100f, "Viet Nam" to 18/1f, "Japan" to 1.0/9f, "Korea" to 1f)
-        val us = mapOf("US" to 1f, "Viet Nam" to 25353f, "Japan" to 152.955f, "Korea" to 1384.25f)
-        val japan = mapOf("US" to 654/100000f, "Viet Nam" to 165.744f, "Japan" to 1f, "Korea" to 9.046f)
+        val m_userinput = findViewById<TextInputEditText>(R.id.abc)
+        val m_buttonSoChan = findViewById<RadioButton>(R.id.radioButton1)
+        val m_buttonSoLe = findViewById<RadioButton>(R.id.radioButton2)
+        val m_buttonSoCP = findViewById<RadioButton>(R.id.radioButton3)
+        val m_errorTextView = findViewById<TextView>(R.id.textView)
+        val m_btnShow = findViewById<Button>(R.id.button)
+        val m_listView = findViewById<ListView>(R.id.listViewShow)
 
+        m_btnShow.setOnClickListener {
+            val a = m_userinput.text.toString()
+            val number = a.toInt()
 
-        val change = mapOf("Viet Nam" to vietnam, "Korea" to korea, "US" to us, "Japan" to japan)
-        var type1 = ""
-        var type2 = ""
-
-        val listItem = arrayOf("Viet Nam", "US", "Japan", "Korea")
-
-        val adapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            listItem
-        )
-
-        val spinner1: Spinner = findViewById(R.id.spinner1)
-        val spinner2: Spinner = findViewById(R.id.spinner2)
-
-        spinner1.adapter = adapter
-        spinner2.adapter = adapter
-
-        val inputText1: EditText = findViewById(R.id.input1)
-        val inputText2: EditText = findViewById(R.id.input2)
-
-        spinner1.onItemSelectedListener = object : NavigationBarView.OnItemSelectedListener, AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                type1 = listItem[p2]
-                inputText1.setText("")
-                inputText2.setText("0")
+            if (a.isEmpty() ||  number <= 0) {
+                m_errorTextView.text = "Khong hop le"
+                return@setOnClickListener
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                type1 = listItem[0]
-            }
 
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                TODO("Not yet implemented")
-            }
-        }
+            m_errorTextView.text = ""
 
-        spinner2.onItemSelectedListener = object : NavigationBarView.OnItemSelectedListener, AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                type2 = listItem[p2]
-                inputText2.setText("")
-                inputText1.setText("0")
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                type2 = listItem[0]
-            }
-
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                TODO("Not yet implemented")
-            }
-        }
-
-        inputText1.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                // Khi editText1 được focus
-                inputText1.setText("")
-                inputText2.setText("0")
-            }
-        }
-
-        inputText2.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                // Khi editText1 được focus
-                inputText2.setText("")
-                inputText1.setText("0")
-            }
-        }
-
-        inputText1.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                if (inputText1.isFocused) {
-                    if (p0 != null) {
-                        if (p0.isNotEmpty()) {
-                            var input1 = p0.toString().toInt()
-                            var rate = change[type1]?.get(type2)?.toFloat()
-                            var num2 = input1 * rate!!
-                            inputText2.setText("$num2")
-                        }
-                    }
+            val res = when {
+                m_buttonSoCP.isChecked -> showSoChinhPhuong(number)
+                m_buttonSoLe.isChecked -> showSoLe(number)
+                m_buttonSoChan.isChecked -> showSoChan(number)
+                else -> {
+                    m_errorTextView.text = "Vui lòng chọn loại số."
+                    return@setOnClickListener
                 }
             }
 
-        })
 
-        inputText2.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                if (inputText2.isFocused) {
-                    if (p0 != null) {
-                        if (p0.isNotEmpty()) {
-                            var input2 = p0.toString().toFloat()
-                            var rate = change[type2]?.get(type1)?.toFloat()
-                            var num1 = input2 * rate!!
-                            inputText1.setText("$num1")
-                        }
-                    }
-                }
-            }
-
-        })
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, res)
+            m_listView.adapter = adapter
+        }
     }
 
+    private fun showSoChan(number: Int): List<Int> {
+        return (0..number).filter { it % 2 == 0 }
+    }
 
+    private fun showSoLe(number: Int): List<Int> {
+        return (0..number).filter { it % 2 != 0 }
+    }
+
+    private fun showSoChinhPhuong(number: Int): List<Int> {
+        return (0..number).filter { isPerfectSquare(it) }
+    }
+
+    private fun isPerfectSquare(num: Int): Boolean {
+        val sqrt = Math.sqrt(num.toDouble()).toInt()
+        return sqrt * sqrt == num
+    }
 }
